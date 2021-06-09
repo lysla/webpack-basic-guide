0cc0f92cb6d97a92d262c6d90bf6092a9800af17
Carmen ~ Lysla <cghirardi@e-project.it>
2021-06-09 12:30:10 +0200

    libs update e adattamento a bootstrap 5

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // Bootstrap Framework
 import './scss/index.scss';
-import 'popper.js';
+import '@popperjs/core';
 import 'bootstrap';
 
 // Tailwind CSS Framework
@@ -10,12 +10,13 @@ import "slick-carousel";
 import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
 
 //#region bootstrap navbar + hamburger + body scroll lock
-$('#navbarMain').on('show.bs.collapse', function (e) {
+var navbarMain = document.getElementById('navbarMain')
+navbarMain.addEventListener('show.bs.collapse', function () {
     $('.navbar-toggler .hamburger').addClass('is-active');
     let scrollableNav = document.querySelector('#navbarMain');
     disableBodyScroll(scrollableNav);
 });
-$('#navbarMain').on('hidden.bs.collapse', function (e) {
+navbarMain.addEventListener('hidden.bs.collapse', function () {
     $('.navbar-toggler .hamburger').removeClass('is-active');
     let scrollableNav = document.querySelector('#navbarMain');
     enableBodyScroll(scrollableNav);
